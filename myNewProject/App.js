7abebc46ea4f8cd65adea42ef21e2656de956e61
kesel <a.kiselyoff@gmail.com>
2023-05-17 21:14:19 +0300

    first test code

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,10 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Text field</Text>
+      <ScrollView style={styles.scroll}>
+        <Text>
+          ScrollView textScrollView textScrollView textScrollView text
+          ScrollView text ScrollView text ScrollView text ScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView textScrollView textScrollView
+          textScrollView textScrollView text ScrollView text ScrollView text
+          ScrollView textScrollView textScrollView textScrollView textScrollView
+          text ScrollView text ScrollView text ScrollView textScrollView
+          textScrollView textScrollView textScrollView text ScrollView text
+          ScrollView text ScrollView text
+        </Text>
+      </ScrollView>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert("Button was pressed")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +92,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  scroll: {
+    flex: 1,
   },
 });
